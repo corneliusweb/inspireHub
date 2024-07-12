1332f6f1c02d5800ae27cda27a80169c3bd9962c
corneliusweb <cornelius.webdev@gmail.com>
2024-07-12 10:02:25 +0100

    js now working fine

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 let openMenu = document.getElementById('topnav-container');
 let button = document.getElementById('menu-btn');
+let close = document.getElementById('close-btn');
 
+//
 button.addEventListener('click', function () {
    openMenu.style.display = 'block';
 });
 
 //
-let close = document.getElementById('close-btn');
 close.addEventListener('click', function () {
    openMenu.style.display = 'none';
 });
@@ -14,20 +15,16 @@ close.addEventListener('click', function () {
 
 
 let errorMsg = document.getElementById('error-message');
-let mail = document.getElementById("email");
-let email = mail.value;
+let email = document.getElementById("email");
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const smileEmoji = '\u{1F600}';
 
 document.getElementById('newsletterForm').addEventListener('submit', function(event) {
    event.preventDefault();
    
-   if(emailPattern.test(email)) {
-      alert(`
-         Thanks for subscribing!${smileEmoji}
-         I would tell you to verify email, but I am still learning.
-      `);
-   }else{
+   if(emailPattern.test(email.value)) {
+      alert(`Thanks for subscribing!🥳 \n I would tell you to verify email, but I am still learning.`);
+      email.value = "";
+   } else {
       errorMsg.style.display = 'block';
    }
 });
