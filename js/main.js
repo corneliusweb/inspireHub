f5affc5c41472f3b14ac990004cc75b4aa8408e0
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-12-26 05:08:21 +0100

    Change email pattern regexp, and rewrite traditional functions in arrow

--- js/main.js
+++ js/main.js
@@ -3,33 +3,34 @@ let button = document.getElementById('menu-btn');
 let close = document.getElementById('close-btn');
 
 //
-button.addEventListener('click', function () {
-   openMenu.style.display = 'block';
+button.addEventListener('click', () => {
+	openMenu.style.display = 'block';
 });
 
 //
-close.addEventListener('click', function () {
-   openMenu.style.display = 'none';
+close.addEventListener('click', () => {
+	openMenu.style.display = 'none';
 });
-
-
 
 let errorMsg = document.getElementById('error-message');
 let email = document.getElementById('email');
-const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const emailPattern = /^\w+([-.]\w+)?(-\w+)?@\w+(-\w+)?\.\w{2,3}(\.\w{2,3})?$/gi;
 
-document.getElementById('newsletterForm').addEventListener('submit', function(event) {
-   event.preventDefault();
-   
-   if(emailPattern.test(email.value)) {
-      //
-      alert(`Thanks for subscribing!🥳 \n This is still up because of the vercel link. Try the netlify link`);
-      this.submit();
+document
+	.getElementById('newsletterForm')
+	.addEventListener('submit', (event) => {
+		event.preventDefault();
 
-      // email.value = "";
-      //
-   } else {
-      errorMsg.style.display = 'block';
-   }
-});
+		if (emailPattern.test(email.value)) {
+			//
+			alert(
+				`Thanks for subscribing!🥳\nThis is still up because of the vercel link. Try the netlify link`
+			);
+			this.submit();
 
+			// email.value = "";
+			//
+		} else {
+			errorMsg.style.display = 'block';
+		}
+	});
